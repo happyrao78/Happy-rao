dde3714bb1dc20412a10192662b56945bffddb27
HAPPY YADAV <happy.hy0017@gmail.com>
2024-04-21 01:38:17 +0530

    vercel inverted comma

--- src/app/(sub pages)/about/page.js
+++ src/app/(sub pages)/about/page.js
@@ -1,71 +1,49 @@
-'use client'
 import Image from "next/image";
 import React from "react";
 import bg from "/public/background/about-background.png";
 import happy from "../../../../public/background/logo.png";
 import AnimatedCursor from "react-animated-cursor";
-// import RenderModel from "../../components/RenderModel";
-// import HatModel from "../../../../public/models/Hat-transformed"
-// import LogoPic from "../../../../public/models/Logo"
-// import Fourth from "../../../../public/models/Four";
 import AboutDetails from "@/app/components/about";
-
 
 export default function Home() {
   return (
     <>
       <AnimatedCursor 
-      innerSize={8}
-      outerSize={30}
-      color="256, 256, 256"
-      outerAlpha={.2}
-      innerScale={0.7}
-      outerScale={3}
-      clickables={[
-        'a',
-        'input[type="text"]',
-        'input[type="email"]',
-        'input[type="number"]',
-        'input[type="submit"]',
-        'input[type="image"]',
-        'label[for]',
-        'select',
-        'textarea',
-        'button',
-        '.link'
-      ]} />
+        innerSize={8}
+        outerSize={30}
+        color="256, 256, 256"
+        outerAlpha={.2}
+        innerScale={0.7}
+        outerScale={3}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+      />
     
-    
-      <Image src={bg} alt ="background-image"  className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"/>
-
+      <Image src={bg} alt="background-image" className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"/>
       
-    
-      <div className="w-full h-3/5 xs:h-3/4 sm:h-screen absolute top-[60%] -translate-y-1/2 left-0">
+      <div className="w-full h-3/5 xs:h-3/4 sm:h-screen absolute top-[60%] -translate-y-1/2 left-0"/>
       
+      <Image priority sizes="100vw" src={happy} alt="background-image" className="-z-50  top-[50%]  w-60 height-60  object-center  justify-center items-center left-1/2 mt-10 opacity-75 "/>
       
-      
-          {/* <HatModel /> */}
-          {/* <Fourth /> */}
-          {/* <LogoPic /> */}
-        
-      
+      <div className="relative w-full h-60 flex flex-col items-center justify-center ">
+        <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[40%] left-1/2 -translate-y-1/2 -translate-x-1/2" >
+          <h1 className="whitespace-nowrap mt-8 text-xl xs:text-2xl sm:text-3xl lg:text-4xl text-accent sanskrit-logo">&quot;सर्वे भवन्तु सुखिनः, सर्वे सन्तु निरामयाः।&quot;</h1>
+          <p className="font-light text-foreground mt-8 text-ls hover:text-accent transition ease-in-out duration-500 sanskrit-logo">~ May all beings be happy, may all beings be free from illness.</p>
+        </div>
       </div>
-      <Image priority sizes="100vw" src={happy} alt ="background-image"  className="-z-50  top-[50%]  w-60 height-60  object-center  justify-center items-center left-1/2 mt-10 opacity-75 "/>
-    <div className="relative w-full h-60 flex flex-col items-center justify-center ">
-    
-    
-    <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[40%] left-1/2 -translate-y-1/2 -translate-x-1/2" >
-    
-
-    
-      <h1 className="whitespace-nowrap mt-8 text-xl xs:text-2xl sm:text-3xl lg:text-4xl text-accent sanskrit-logo">"सर्वे भवन्तु सुखिनः, सर्वे सन्तु निरामयाः।"</h1>
-      <p className="font-light text-foreground mt-8 text-ls hover:text-accent transition ease-in-out duration-500 sanskrit-logo">~ May all beings be happy, may all beings be free from illness.</p>
       
-    </div>
-    </div>
-    <AboutDetails />
+      <AboutDetails />
     </>
   );
 }
-
-   
--- src/app/(sub pages)/contact/page.js
+++ src/app/(sub pages)/contact/page.js
@@ -1,66 +1,49 @@
-'use client'
 import Image from "next/image";
 import React from "react";
 import { CiVirus } from "react-icons/ci";
 import bg from "/public/background/contact-background.png";
-// import happy from "../../../../public/background/logo.png";
 import AnimatedCursor from "react-animated-cursor";
 import Form from "../../components/contact/form";
+
 export default function Contact() {
   return (
     <>
       <AnimatedCursor 
-      innerSize={8}
-      outerSize={30}
-      color="256, 256, 256"
-      outerAlpha={.2}
-      innerScale={0.7}
-      outerScale={3}
-      clickables={[
-        'a',
-        'input[type="text"]',
-        'input[type="email"]',
-        'input[type="number"]',
-        'input[type="submit"]',
-        'input[type="image"]',
-        'label[for]',
-        'select',
-        'textarea',
-        'button',
-        '.link'
-      ]} />
+        innerSize={8}
+        outerSize={30}
+        color="256, 256, 256"
+        outerAlpha={.2}
+        innerScale={0.7}
+        outerScale={3}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link'
+        ]}
+      />
     
-    
-      <Image priority sizes="100vw" src={bg} alt ="background-image"  className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"/>
-
-      
-     
-      
-      
-{/*       
-      <Image src={happy} alt ="background-image"  className="-z-50 fixed top-4 left-10 hidden xs:h-20 xs:w-20 sm:h-30 sm:w-30 md:h-40 md:w-40 object-cover object-center opacity-70 justify-center items-center "/> */}
+      <Image priority sizes="100vw" src={bg} alt="background-image" className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"/>
       
       <article className="relative w-full flex flex-col items-center justify-center py-8 sm:py-0 space-y-8">
         <div className="flex flex-col items-center justify-center space-y-6 w-full sm:w-3/4">
-        <div className="flex gap-10 w-auto ">
-        <CiVirus className="text-white text-8xl hover:text-accent transition ease-in-out duration-250 motion-safe:animate-spin-slow" />
-
-        <CiVirus className="text-white text-8xl hover:text-accent transition ease-in-out duration-250 motion-safe:animate-spin-slow" />
-
-
-        <CiVirus className="text-white text-8xl hover:text-accent transition ease-in-out duration-250 motion-safe:animate-spin-slow" />
-
-        </div>
-        
-
-          <h1 className=" whitespace-nowrap text-accent font-semibold tex-center text-4xl capitalize">Contact Happy Yadav</h1>
-          <p className="text-center font-light text-sm xs:text-base">Let's make a worth connection ! Drop your queries / message below and I'll reply to it as early as possible.</p>
+          <div className="flex gap-10 w-auto ">
+            <CiVirus className="text-white text-8xl hover:text-accent transition ease-in-out duration-250 motion-safe:animate-spin-slow" />
+            <CiVirus className="text-white text-8xl hover:text-accent transition ease-in-out duration-250 motion-safe:animate-spin-slow" />
+            <CiVirus className="text-white text-8xl hover:text-accent transition ease-in-out duration-250 motion-safe:animate-spin-slow" />
+          </div>
+          <h1 className="whitespace-nowrap text-accent font-semibold text-center text-4xl capitalize">Contact Happy Yadav</h1>
+          <p className="text-center font-light text-sm xs:text-base">Let&apos;s make a worth connection! Drop your queries / message below and I&apos;ll reply to it as early as possible.</p>
         </div>
         <Form />
       </article>
-    
     </>
   );
 }
-
-   
