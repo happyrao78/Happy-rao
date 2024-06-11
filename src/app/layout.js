f2f281b606537c0f3a6bf0400a72f83bb99e4c2d
HAPPY YADAV <happy.hy0017@gmail.com>
2024-06-11 14:28:21 +0530

    enabled google search console

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,11 +11,21 @@ variable: "--font-inter",
 export const metadata = {
   title: "Happy Yadav",
   description: "This is a personal portfolio website made using react, next.js, tailwindcss, three.js, emailjs. 🖥️⚙️",
+  name: "google-site-verification",
+  content: "xib0rnzpGcFXRULeLaNYEFJCECscHeA2Scqv3rGJtfQ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description} />
+        <title>{metadata.title}</title>
+        <meta name="google-site-verification" content={metadata.content} />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+    
       <body className={clsx(inter.variable, "bg-background text-foreground font-inter")}>{children}
       <FireFliesBackground />
       <Sound />
